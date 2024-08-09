77bf5ced5b9569912ace54160403349bc5af8152
Claire711 <2147113864@qq.com>
2024-08-09 10:11:08 +0800

    定义一个不转义相应内容的safe模板函数

--- gin_practice/main.go
+++ gin_practice/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
 )
 
 //定义模板
@@ -9,12 +10,18 @@ import (
 func main() {
 	r := gin.Default()
 
+	//在解析模板之前，告诉电脑，我还有自定义的部分
+	r.SetFuncMap(template.FuncMap{
+		"safe": func(str string) template.HTML {
+			return template.HTML(str)
+		}, //这里需要在后面加上，
+	})
 	//解析模板
 	r.LoadHTMLFiles("./test.tmpl")
 	//渲染模板
 	//relativepath 后跟的是端口后面的网址内容
 	r.GET("/test", func(c *gin.Context) {
-		c.HTML(200, "test.tmpl", "<a>claire的博客</a>")
+		c.HTML(200, "test.tmpl", "<a href='https://liwenzhou.com'>李文周的博客</a>")
 	})
 	//启动server
 	r.Run(":8080")
