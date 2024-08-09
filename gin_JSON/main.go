7349a8f942b31b9cd4df6ed1de8baf0c29c89bfa
Claire711 <2147113864@qq.com>
2024-08-09 11:29:16 +0800

    JSON渲染gin框架,结构体定义

--- gin_JSON/main.go
+++ gin_JSON/main.go
@@ -4,14 +4,32 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/json", func(c *gin.Context) {
-		//interface后是空接口，表示可以是任意类型的数据格式
-		data := map[string]interface{}{
-			"name":    "claire",
-			"message": "hello world",
-			"age":     20,
+	/*	r.GET("/json", func(c *gin.Context) {*/
+	//interface后是空接口，表示可以是任意类型的数据格式
+	//data := map[string]interface{}{
+	//	"name":    "claire",
+	//	"message": "hello world",
+	//	"age":     20,
+	//	}
+	/*	data := gin.H{
+		"name":    "claire",
+		"message": "hello world",
+		"age":     20,
+	}*/
+	//方法2 结构体
+	type msg struct {
+		Name string
+		Age  int
+		Msg  string
+	}
+	r.GET("/other", func(c *gin.Context) {
+		data := msg{
+			Name: "claire",
+			Age:  20,
+			Msg:  "hello world",
 		}
 		c.JSON(200, data)
 	})
+
 	r.Run(":9000")
 }
